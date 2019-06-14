a946831932271635d16debd13372e29c1778ee2f
Otávio do Prado Martins <otavio.pradoM@hotmail.com>
2019-06-14 19:40:38 -0300

    Finalizado a versão desktop e tablet.

---
+++ js/script_jquery.js
@@ -0,0 +1,191 @@
+$(document).ready(function() {
+	$(".abrirProfessor1").hide();
+	$(".abrirProfessor2").hide();
+	$(".abrirProfessor3").hide();
+	$(".abrirProfessor4").hide();
+	$(".abrirProfessor5").hide();
+	$(".abrirProfessor6").hide();
+	$(".abrirProfessor7").hide();
+	$(".abrirProfessor8").hide();
+
+	$(".professor1").click(function() {
+		$(".abrirProfessor1").show();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor2").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").show();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor3").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor4").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").show();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor5").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").show();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor6").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").show();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor7").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").show();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor8").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").show();
+	});
+
+	$(".professor9").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor10").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").hide();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").show();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+
+	$(".professor11").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+	$(".professor12").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+	$(".professor13").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+	$(".professor14").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+	$(".professor15").click(function() {
+		$(".abrirProfessor1").hide();
+		$(".abrirProfessor2").hide();
+		$(".abrirProfessor3").show();
+		$(".abrirProfessor4").hide();
+		$(".abrirProfessor5").hide();
+		$(".abrirProfessor6").hide();
+		$(".abrirProfessor7").hide();
+		$(".abrirProfessor8").hide();
+	});
+});
+
+jQuery(document).ready(function() {
+
+        jQuery("#subirTopo").hide();
+
+        jQuery('a#subirTopo').click(function() {
+            jQuery('html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        jQuery(window).scroll(function() {
+            if (jQuery(this).scrollTop() > 1000) {
+                jQuery('#subirTopo').fadeIn();
+            } else {
+                jQuery('#subirTopo').fadeOut();
+            }
+        });
+    });
